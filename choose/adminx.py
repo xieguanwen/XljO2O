462f386ace2acf545fbc39f0c90d6d91c0c582f7
xiegw <xiegw@xiaolajiao.com>
2015-03-23 17:18:57 +0800

    评选

--- choose/adminx.py
+++ choose/adminx.py
@@ -1,12 +1,25 @@
 import xadmin
-from xadmin import views
+from choose.models import SelectedTheme
+from choose.models import ClerkAward
+from choose.models import StoreAward
 
-# class GlobalSetting(object):
-#     global_search_models = [Host, IDC]
-#     global_models_icon = {
-#         # Host: 'fa fa-laptop', IDC: 'fa fa-cloud'
-#     }
-#     menu_style = 'default'#'accordion'
-# xadmin.site.register(views.CommAdminView, GlobalSetting)
+class SelectedThemeAdmin(object):
+    list_display = ("selectedThemeId","name")
+    list_display_links = ("name",)
+    # search_fields = ()
 
-# xadmin.site.register(AccessRecord, AccessRecordAdmin)
+xadmin.site.register(SelectedTheme,SelectedThemeAdmin)
+
+class ClerkAwardAdmin(object):
+    list_display = ("clerkAwardId","clerkId")
+    list_display_links = ("clerkId",)
+    # search_fields = ()
+
+xadmin.site.register(ClerkAward,ClerkAwardAdmin)
+
+class StoreAwardAdmin(object):
+    list_display = ("storeAwardId","storeId")
+    list_display_links = ("storeId",)
+    # search_fields = ()
+
+xadmin.site.register(StoreAward,StoreAwardAdmin)
--- choose/models.py
+++ choose/models.py
@@ -4,22 +4,25 @@ from stores.models import Store
 from stores.models import Clerk
 
 class SelectedTheme(models.Model):
-    selectedThemeId = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-    period = models.IntegerField()
-    type = models.SmallIntegerField()
+    TYPE = ((1,"周"),(2,"月"),(3,"年"))
+    selectedThemeId = models.AutoField("主题编号",primary_key=True)
+    name = models.CharField("主题",max_length=200)
+    period = models.IntegerField("周期数")
+    type = models.SmallIntegerField("类型",choices=TYPE,default=TYPE[0][0],help_text="1：为周，2：为月，3：为年")
 
     def __unicode__(self):
         return self.name
     class Meta:
         db_table = "selected_theme"
         app_label = "choose"
+        verbose_name = "评选主题"
+        verbose_name_plural = "评选主题"
 
 class StoreAward(models.Model):
-    storeAwardId = models.AutoField(primary_key=True)
-    storeId = models.ForeignKey(Store,db_column="storeId")
-    selectedThemeId = models.ForeignKey(SelectedTheme,db_column="selectedThemeId")
-    overallScore = models.IntegerField()
+    storeAwardId = models.AutoField("编号",primary_key=True)
+    storeId = models.ForeignKey(Store,db_column="storeId",verbose_name="店铺")
+    selectedThemeId = models.ForeignKey(SelectedTheme,db_column="selectedThemeId",verbose_name="主题")
+    overallScore = models.IntegerField("综合分数")
 
     def __unicode__(self):
         return self.selectedThemeId
@@ -27,12 +30,14 @@ class StoreAward(models.Model):
     class Meta:
         db_table = "store_award"
         app_label = "choose"
+        verbose_name = "店铺评选"
+        verbose_name_plural = "店铺评选"
 
 class ClerkAward(models.Model):
-    id = models.AutoField(primary_key=True)
-    selectedThemeId = models.ForeignKey(SelectedTheme,db_column="selectedThemeId")
-    clerkId = models.ForeignKey(Clerk,db_column="clerkId")
-    overallScore = models.IntegerField()
+    id = models.AutoField("编号",primary_key=True)
+    selectedThemeId = models.ForeignKey(SelectedTheme,db_column="selectedThemeId",verbose_name="主题")
+    clerkId = models.ForeignKey(Clerk,db_column="clerkId",verbose_name="店员")
+    overallScore = models.IntegerField("综合分数")
 
     def __unicode__(self):
         return self.clerkId
@@ -40,3 +45,5 @@ class ClerkAward(models.Model):
     class Meta:
         db_table = "clerk_award"
         app_label = "choose"
+        verbose_name = "店员评选"
+        verbose_name_plural = "店员评选"
