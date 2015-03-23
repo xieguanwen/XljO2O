293f2888cb0c9e772c34b6b00aafe82a957af7bf
xieguanwen <xieguanwen@gmail.com>
2015-03-23 15:08:39 +0800

    Revert "Development"

--- notice/adminx.py
+++ notice/adminx.py
@@ -1,6 +1,18 @@
 import xadmin
 from xadmin import views
 
+class MainDashboard(object):
+    widgets = [
+    ]
+xadmin.site.register(views.website.IndexView, MainDashboard)
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+
+
 # class GlobalSetting(object):
 #     global_search_models = [Host, IDC]
 #     global_models_icon = {
--- user/models.py
+++ user/models.py
@@ -3,19 +3,18 @@ import datetime
 from django.db import models
 
 class User(models.Model):
-    RANK = ((1,"店铺"),(2,"代理商"))
-    userId = models.AutoField("用户编号",primary_key=True)
-    userName = models.CharField("用户名称",max_length=50)
-    password = models.CharField("密码",max_length=32)
-    addTime = models.DateTimeField("添加时间",default=datetime.datetime.now())
-    rank = models.SmallIntegerField("等级",choices=RANK,default=RANK[0][0])
-    email = models.CharField("电子邮件",max_length=50)
-    sex = models.CharField("性别",max_length=10)
-    integration = models.IntegerField("积分",default=0,blank=True)
-    lastLogin = models.DateTimeField("最后登录时间",blank=True)
-    visitCount = models.IntegerField("登录次数",blank=True)
-    mobile = models.CharField("手机",max_length=18)
-    tel = models.CharField("电话",max_length=20,blank=True)
+    userDd = models.AutoField(primary_key=True)
+    userName = models.CharField(max_length=50)
+    password = models.CharField(max_length=32)
+    addTime = models.DateTimeField(default=datetime.datetime.now())
+    rank = models.SmallIntegerField(default=(1,2))
+    email = models.CharField(max_length=50)
+    sex = models.CharField(max_length=10)
+    integration = models.IntegerField(default=0,blank=True)
+    lastLogin = models.DateTimeField(blank=True)
+    visitCount = models.IntegerField()
+    mobile = models.CharField(max_length=18)
+    tel = models.CharField(max_length=20)
 
     def __unicode__(self):
         return self.userName
@@ -23,8 +22,6 @@ class User(models.Model):
     class Meta():
         db_table = "user"
         app_label = "user"
-        verbose_name = "用户"
-        verbose_name_plural = "用户"
 
 
 
