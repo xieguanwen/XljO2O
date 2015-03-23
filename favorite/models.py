5dfad9689104844f3413eeb66a3008f37bb79fcd
xiegw <xiegw@xiaolajiao.com>
2015-03-23 18:13:19 +0800

    点赞

--- favorite/models.py
+++ favorite/models.py
@@ -4,11 +4,11 @@ from stores.models import Store
 from stores.models import Clerk
 
 class Favorite(models.Model):
-    favoriteId = models.AutoField(primary_key=True)
-    storeId = models.ForeignKey(Store,db_column="storeId")
-    user_id = models.IntegerField()
-    favoriteMobile = models.CharField(max_length=20)
-    clerkId = models.ForeignKey(Clerk,db_column="clerkId")
+    favoriteId = models.AutoField("编号",primary_key=True)
+    storeId = models.ForeignKey(Store,db_column="storeId",verbose_name="店铺")
+    user_id = models.IntegerField("官网用户")
+    favoriteMobile = models.CharField("点赞手机",max_length=20)
+    clerkId = models.ForeignKey(Clerk,db_column="clerkId",verbose_name="店员")
 
     def __unicode__(self):
         return self.favoriteMobile
@@ -16,6 +16,8 @@ class Favorite(models.Model):
     class Meta:
         db_table = "favorite"
         app_label = "favorite"
+        verbose_name = "点赞"
+        verbose_name_plural = "点赞"
 
 
 
