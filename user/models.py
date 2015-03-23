a40efa3412e3b64919228b56523c83122c88cb05
xiegw <xiegw@xiaolajiao.com>
2015-03-23 14:30:57 +0800

    修改rank

--- user/models.py
+++ user/models.py
@@ -3,12 +3,12 @@ import datetime
 from django.db import models
 
 class User(models.Model):
-    RANK = (("代理商",2),("店铺",1))
+    RANK = ((1,"店铺"),(2,"代理商"))
     userId = models.AutoField("用户编号",primary_key=True)
     userName = models.CharField("用户名称",max_length=50)
     password = models.CharField("密码",max_length=32)
     addTime = models.DateTimeField("添加时间",default=datetime.datetime.now())
-    rank = models.SmallIntegerField("等级",default=RANK[1][1],choices=RANK)
+    rank = models.SmallIntegerField("等级",choices=RANK,default=RANK[1][0])
     email = models.CharField("电子邮件",max_length=50)
     sex = models.CharField("性别",max_length=10)
     integration = models.IntegerField("积分",default=0,blank=True)
