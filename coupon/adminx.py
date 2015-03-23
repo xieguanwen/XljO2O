6a4a84b5b6624c5a027dc5ec49c2840038f88aec
xiegw <xiegw@xiaolajiao.com>
2015-03-23 18:09:32 +0800

    ElectronicCoupons

--- coupon/adminx.py
+++ coupon/adminx.py
@@ -3,7 +3,7 @@ from coupon.models import ECouponCat
 from coupon.models import ElectronicCoupons
 
 class ECouponCatAdmin(object):
-    list_display = ("eCouponCatId","name")
+    list_display = ("ECouponCatId","name")
     list_display_links = ("name",)
     # search_fields = ()
 
