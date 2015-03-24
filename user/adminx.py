871aa0134f3433a47a5aa80291b58da51f57fcff
xiegw <xiegw@xiaolajiao.com>
2015-03-24 11:01:29 +0800

    main menu

--- user/adminx.py
+++ user/adminx.py
@@ -1,7 +1,8 @@
 import xadmin
 from xadmin import views
-from django.contrib import admin
 from models import User
+from stores.models import Store
+from agents.models import Agents
 
 # class MainDashboard(object):
 #     widgets = [
@@ -9,10 +10,19 @@ from models import User
 # xadmin.site.register(views.website.IndexView, MainDashboard)
 
 
-# class BaseSetting(object):
-#     enable_themes = True
-#     use_bootswatch = True
-# xadmin.site.register(views.BaseAdminView, BaseSetting)
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+
+class GlobalSetting(object):
+    global_search_models = [User, Store , Agents]
+    global_models_icon = {
+        User: 'fa fa-laptop', Store: 'fa fa-cloud'
+    }
+    menu_style = 'default'
+xadmin.site.register(views.CommAdminView, GlobalSetting)
+
 
 class UserAdmin(object):
     list_display = ("userId","userName")
