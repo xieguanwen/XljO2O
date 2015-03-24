2497833239e225fc8a982cd3bdd7e58eea197170
xiegw <xiegw@xiaolajiao.com>
2015-03-24 17:27:29 +0800

    upload 测试

--- agents/models.py
+++ agents/models.py
@@ -7,7 +7,7 @@ class Agents(models.Model):
     agentsId = models.AutoField("代理商编号",primary_key=True)
     userId = models.ForeignKey(User,db_column="userId",verbose_name="用户名")
     companyName = models.CharField("公司名称",max_length=50)
-    license = models.CharField("营业执照",max_length=100)
+    license = models.FileField("营业执照",upload_to="../images/uploads/")
     certificate = models.CharField("授权证书",max_length=100)
     addTime = models.DateTimeField("添加时间",default=datetime.datetime.now())
 
