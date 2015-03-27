f33cc5cdfaaee17fdebcf4cb3eebadb062d9916f
xiegw <xiegw@xiaolajiao.com>
2015-03-27 18:20:18 +0800

    "活动"

--- region/models.py
+++ region/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Region(models.Model):
     TYPE = ((0,"国家"),(1,"省"),(2,"市"),(3,"地区"))
     regionId = models.AutoField("编号",primary_key=True)
-    parentId = models.ManyToManyField("self",db_column="regionId",verbose_name="父类编号")
+    parentId = models.ForeignKey("self",db_column="regionId",verbose_name="父类编号")
     name = models.CharField("名称",max_length=50)
     type = models.SmallIntegerField("类型",default=TYPE[3][0],choices=TYPE)
 
