0cccc9e7439b2d7fc81574ecdba9a74120774368
xiegw <xiegw@xiaolajiao.com>
2015-04-02 10:35:14 +0800

    app_label

--- agents/models.py
+++ agents/models.py
@@ -6,10 +6,13 @@ from user.models import User
 class Agents(models.Model):
     agentsId = models.AutoField("代理商编号",primary_key=True)
     userId = models.ForeignKey(User,db_column="userId",verbose_name="用户名")
+    agentsRegion = models.CharField("代理商省区",max_length=50,help_text="例如：北京。也可以用多个")
+    contact = models.CharField("联系人",max_length=50)
     companyName = models.CharField("公司名称",max_length=50)
     license = models.FileField("营业执照",upload_to="./images/uploads/")
     certificate = models.FileField("授权证书",upload_to="./images/uploads/")
     addTime = models.DateTimeField("添加时间",default=datetime.datetime.now())
+    tel = models.CharField("电话",max_length=30,blank=True)
 
     def __unicode__(self):
         return self.companyName
