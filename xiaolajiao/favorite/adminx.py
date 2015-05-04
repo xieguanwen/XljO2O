17f97c7b6eeb043b5e7f2ed8cf65d2394e3a7fd9
xiegw <xiegw@xiaolajiao.com>
2015-05-04 16:20:52 +0800

    modify clerkFavoriteId

--- xiaolajiao/favorite/adminx.py
+++ xiaolajiao/favorite/adminx.py
@@ -2,7 +2,7 @@ import xadmin
 from xiaolajiao.favorite.models import Favorite
 
 class FavoriteAdmin(object):
-    list_display = ("favoriteId","user_id")
+    list_display = ("clerkFavoriteId","user_id")
     list_display_links = ("user_id",)
     # search_fields = ()
 
