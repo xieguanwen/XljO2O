6f205ccdf9da7bec9f47a730c15301674e3bca0a
xiegw <xiegw@xiaolajiao.com>
2015-05-05 11:09:40 +0800

    测试

--- xiaolajiao/user/forms.py
+++ xiaolajiao/user/forms.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.utils.translation import ugettext, ugettext_lazy as _
+from xiaolajiao.user.models import User
 
 class UserCreationForm(forms.ModelForm):
     password = forms.CharField(label=_("密码"),widget=forms.PasswordInput)
+
+
+    class Meta:
+        Model = User
