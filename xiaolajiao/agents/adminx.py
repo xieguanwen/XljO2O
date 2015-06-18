31a3547341da492a2a2d9b7cd9177614d46e4b77
xiegw <xiegw@xiaolajiao.com>
2015-06-18 14:16:00 +0800

    修改了raw_id_fields

--- xiaolajiao/agents/adminx.py
+++ xiaolajiao/agents/adminx.py
@@ -4,8 +4,9 @@ from xiaolajiao.agents.models import Agents
 class AgentsAdmin(object):
     list_display = ("agentsId","companyName")
     list_display_links = ("companyName",)
-    search_fields = ("companyName","contact","userId__userName")
+    search_fields = ("companyName","contact")
     raw_id_fields = ("userId",)
+    show_detail_fields = ('userId',)
 
 
 xadmin.site.register(Agents,AgentsAdmin)
