34357aaf17aad2f16e843d57c4fb7e167b0836a8
xiegw <xiegw@xiaolajiao.com>
2015-07-02 12:05:35 +0800

    修改region

--- xiaolajiao/region/models.py
+++ xiaolajiao/region/models.py
@@ -23,7 +23,7 @@ class Province(models.Model):
 class City(models.Model):
     TYPE = ((0,"国家"),(1,"省"),(2,"市"),(3,"地区"))
     city = models.AutoField("编号",primary_key=True)
-    province = models.ForeignKey(Province,db_column="province_id",verbose_name="父类编号")
+    province = models.ForeignKey(Province,db_column="province",verbose_name="父类编号")
     region_name = models.CharField("名称",max_length=50)
     region_type = models.SmallIntegerField("类型",default=TYPE[3][0],choices=TYPE)
     agency_id = models.SmallIntegerField("没有作用",blank=True,editable=False)
@@ -41,7 +41,7 @@ class City(models.Model):
 class Region(models.Model):
     TYPE = ((0,"国家"),(1,"省"),(2,"市"),(3,"地区"))
     region = models.AutoField("编号",primary_key=True)
-    city = models.ForeignKey(City,db_column="city_id",verbose_name="父类编号")
+    city = models.ForeignKey(City,db_column="city",verbose_name="父类编号")
     region_name = models.CharField("名称",max_length=50)
     region_type = models.SmallIntegerField("类型",default=TYPE[3][0],choices=TYPE)
     agency_id = models.SmallIntegerField("没有作用",blank=True,editable=False)
