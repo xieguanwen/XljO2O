21238f4bd1e7c79434b8e90529cd6fe15677fa52
xiegw <xiegw@xiaolajiao.com>
2015-07-02 11:16:15 +0800

    修改region

--- xiaolajiao/region/adminx.py
+++ xiaolajiao/region/adminx.py
@@ -2,8 +2,8 @@ import xadmin
 from xiaolajiao.region.models import Region
 
 class RegionAdmin(object):
-    list_display = ("regionId","name")
-    list_display_links = ("name",)
+    list_display = ("region_id","region_name")
+    list_display_links = ("region_name",)
     # search_fields = ()
 
 # xadmin.site.register(Region,RegionAdmin)
--- xiaolajiao/region/models.py
+++ xiaolajiao/region/models.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-class Region(models.Model):
+
+class Province(models.Model):
     TYPE = ((0,"国家"),(1,"省"),(2,"市"),(3,"地区"))
     region_id = models.AutoField("编号",primary_key=True)
-    parent_id = models.ForeignKey(City,db_column="region_id",verbose_name="父类编号")
+    parent_id = models.IntegerField("父类编号",default=0)
     region_name = models.CharField("名称",max_length=50)
     region_type = models.SmallIntegerField("类型",default=TYPE[3][0],choices=TYPE)
     agency_id = models.SmallIntegerField("没有作用",blank=True,editable=False)
@@ -13,10 +14,11 @@ class Region(models.Model):
         return self.region_name
 
     class Meta:
-        db_table = "region"
+        db_table = "province"
         app_label = "region"
         verbose_name = "地区"
         verbose_name_plural = "地区"
+
 
 class City(models.Model):
     TYPE = ((0,"国家"),(1,"省"),(2,"市"),(3,"地区"))
@@ -35,10 +37,11 @@ class City(models.Model):
         verbose_name = "地区"
         verbose_name_plural = "地区"
 
-class Province(models.Model):
+
+class Region(models.Model):
     TYPE = ((0,"国家"),(1,"省"),(2,"市"),(3,"地区"))
     region_id = models.AutoField("编号",primary_key=True)
-    parent_id = models.IntegerField("父类编号",default=0)
+    parent_id = models.ForeignKey(City,db_column="region_id",verbose_name="父类编号")
     region_name = models.CharField("名称",max_length=50)
     region_type = models.SmallIntegerField("类型",default=TYPE[3][0],choices=TYPE)
     agency_id = models.SmallIntegerField("没有作用",blank=True,editable=False)
@@ -47,10 +50,11 @@ class Province(models.Model):
         return self.region_name
 
     class Meta:
-        db_table = "province"
+        db_table = "region"
         app_label = "region"
         verbose_name = "地区"
         verbose_name_plural = "地区"
+
 
 
 
