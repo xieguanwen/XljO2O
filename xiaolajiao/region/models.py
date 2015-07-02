70c237ba07babd35d00413e1e166746880577fba
xiegw <xiegw@xiaolajiao.com>
2015-07-02 12:04:09 +0800

    修改region

--- xiaolajiao/region/models.py
+++ xiaolajiao/region/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Province(models.Model):
     TYPE = ((0,"国家"),(1,"省"),(2,"市"),(3,"地区"))
-    province_id = models.AutoField("编号",primary_key=True)
-    parent_id = models.IntegerField("父类编号",default=0)
+    province = models.AutoField("编号",primary_key=True)
+    parent = models.IntegerField("父类编号",default=0)
     region_name = models.CharField("名称",max_length=50)
     region_type = models.SmallIntegerField("类型",default=TYPE[3][0],choices=TYPE)
     agency_id = models.SmallIntegerField("没有作用",blank=True,editable=False)
@@ -22,8 +22,8 @@ class Province(models.Model):
 
 class City(models.Model):
     TYPE = ((0,"国家"),(1,"省"),(2,"市"),(3,"地区"))
-    city_id = models.AutoField("编号",primary_key=True)
-    province_id = models.ForeignKey(Province,db_column="province_id",verbose_name="父类编号")
+    city = models.AutoField("编号",primary_key=True)
+    province = models.ForeignKey(Province,db_column="province_id",verbose_name="父类编号")
     region_name = models.CharField("名称",max_length=50)
     region_type = models.SmallIntegerField("类型",default=TYPE[3][0],choices=TYPE)
     agency_id = models.SmallIntegerField("没有作用",blank=True,editable=False)
@@ -40,8 +40,8 @@ class City(models.Model):
 
 class Region(models.Model):
     TYPE = ((0,"国家"),(1,"省"),(2,"市"),(3,"地区"))
-    region_id = models.AutoField("编号",primary_key=True)
-    city_id = models.ForeignKey(City,db_column="city_id",verbose_name="父类编号")
+    region = models.AutoField("编号",primary_key=True)
+    city = models.ForeignKey(City,db_column="city_id",verbose_name="父类编号")
     region_name = models.CharField("名称",max_length=50)
     region_type = models.SmallIntegerField("类型",default=TYPE[3][0],choices=TYPE)
     agency_id = models.SmallIntegerField("没有作用",blank=True,editable=False)
