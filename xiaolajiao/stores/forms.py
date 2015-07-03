a8d4a03a3e77cfabddfdb7b857d389c2e4444c39
xiegw <xiegw@xiaolajiao.com>
2015-07-03 14:48:08 +0800

    test

--- xiaolajiao/stores/forms.py
+++ xiaolajiao/stores/forms.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
+import sys
 from django import forms
 from xiaolajiao.stores.models import StoreTemp
+from xiaolajiao.stores.models import Store
 
 class StoresTempChangeForm(forms.ModelForm):
     class Meta:
@@ -9,9 +11,14 @@ class StoresTempChangeForm(forms.ModelForm):
 
     def save(self, commit=True):
         storesTemp = super(StoresTempChangeForm, self).save(commit=False)
-        logging.basicConfig(filename = "/tmp/xiaolajiao.log", level = logging.INFO)
-        logging.info(storesTemp.logType)
-        logging.info(storesTemp.storeId)
+        # logging.basicConfig(filename = "/tmp/xiaolajiao.log", level = logging.INFO)
+        # logging.info(storesTemp.logType)
+        # logging.info(storesTemp.storeId)
+        # Store.objects;
+        if(storesTemp.storeId is None):
+            store = Store.__init__()
+        else:
+            pass
         if commit:
             storesTemp.save()
         return storesTemp
