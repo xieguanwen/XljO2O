8efcd2c194fd96297ffb8819b0eb1800f005f8b4
xiegw <xiegw@xiaolajiao.com>
2015-11-10 14:52:06 +0800

    修改tac

--- xiaolajiao/product/adminx.py
+++ xiaolajiao/product/adminx.py
@@ -1,6 +1,7 @@
 import xadmin
 from xiaolajiao.product.models import ProductSales
 from xiaolajiao.product.models import ProductColor
+from xiaolajiao.product.models import ProductType
 
 class ProductSalesAdmin(object):
     list_display = ("productSalesId","imei","productName","addTime")
@@ -15,3 +16,10 @@ class ProductColorAdmin(object):
     # search_fields = ()
 
 xadmin.site.register(ProductColor,ProductColorAdmin)
+
+class ProductTypeAdmin(object):
+    list_display = ("tacCode","name")
+    list_display_links = ("name",)
+    # search_fields = ()
+
+xadmin.site.register(ProductType,ProductTypeAdmin)
--- xiaolajiao/user/models.py
+++ xiaolajiao/user/models.py
@@ -21,7 +21,7 @@ class User(models.Model):
     integration = models.IntegerField("积分",default=0,blank=True)
     lastLogin = models.DateTimeField("最后登录时间",blank=True)
     visitCount = models.IntegerField("登录次数",blank=True)
-    mobile = models.CharField("手机",max_length=18,unique=True)
+    mobile = models.CharField("手机",max_length=18,blank=True)
     tel = models.CharField("电话",max_length=20,blank=True)
     agentsId = models.IntegerField('代理商编号',blank=True,editable=True)
 
